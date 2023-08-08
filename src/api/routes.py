221ed20483f77fdd6daa119231f0ed2130eb7f52
Franchesca Zegarra <129009072+Khrisefzm@users.noreply.github.com>
2023-08-08 19:25:15 +0000

    add singup and login routes, but just testing

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,8 +5,21 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
+# Flask jwt
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+# Flask Bcrypt
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+
 api = Blueprint('api', __name__)
 
+#Flask Bcrypt
+bcrypt = Bcrypt(app)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -16,3 +29,34 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/singup', methods = ['POST'])
+def create_user():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    #encrypt password:
+    pw_hash = bcrypt.generate_password_hash(password)
+    #create new user
+    new_user = User(email=email, password=pw_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 200
+
+@api.route("/login", methods=["POST"])
+def login():
+    password_from_database = 'esta es mi contraseña'
+    # Hash pasword, only for test, in the data base the password is encrypt:
+    db_pw_hash = bcrypt.generate_password_hash(password_from_database)
+
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # Hash pasword:
+    pw_hash = bcrypt.generate_password_hash(password)
+
+    if email != "test" or bcrypt.check_password_hash(db_pw_hash, pw_hash):
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+
